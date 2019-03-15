5a8e2aa04fefca251a88a95f8d6a3b5b165ea36d
beast <459605823@qq.com>
2019-03-15 23:02:09 +0800

    add sidebar

--- src/pages/home/components/header.vue
+++ src/pages/home/components/header.vue
@@ -1,7 +1,8 @@
 <template>
+   <div>
     <div class="header">
         <div class="home-header">
-            <img src="https://img1.qunliao.info/fastdfs4/M00/B1/6D/100x100/-/-/ChMf8FxIslCANJOdAAAok1n7B7I893.jpg" alt="" class="user-avatar">
+            <img src="https://img1.qunliao.info/fastdfs4/M00/B1/6D/100x100/-/-/ChMf8FxIslCANJOdAAAok1n7B7I893.jpg" alt="" class="user-avatar" @click="showSidebar">
             <h1>懂球帝</h1>
         </div>
         <div class="home-nav">
@@ -10,6 +11,38 @@
             </ul>
         </div>
     </div>
+    <div class="shadow" ref="shadow" @click="hideSidebar"></div>
+    <div class="sidebar" ref="sidebar">
+        <div class="sidebar-header">
+            <!-- <div class="cover"></div> -->
+            <div class="header-top">
+                <img class="sidebar-avatar" src="https://img1.qunliao.info/fastdfs4/M00/B1/6D/100x100/-/-/ChMf8FxIslCANJOdAAAok1n7B7I893.jpg" alt="">
+                <div class="userinfo">
+                    <h4>username</h4>
+                    <p class="days">已成为懂球帝1788天</p>
+                </div>
+            </div>
+            <div class="header-nav">
+                <div class="nav-item">
+                    <p class="nav-number">0</p>
+                    <p class="nav-type">关注</p>
+                </div>
+                <div class="dash"></div>
+                <div class="nav-item">
+                    <p class="nav-number">0</p>
+                    <p class="nav-type">关注</p>
+                </div>
+                <div class="dash"></div>
+                <div class="nav-item">
+                    <p class="nav-number">0</p>
+                    <p class="nav-type">关注</p>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar-content">
+        </div>
+    </div>
+   </div>
 </template>
 
 <script>
@@ -18,6 +51,16 @@ export default {
   data () {
     return {
       list: ['头条', '热门', '视频', '西甲', '意甲', '德甲', '集锦']
+    }
+  },
+  methods: {
+    showSidebar () {
+      this.$refs.sidebar.style.left = 0
+      this.$refs.shadow.style.display = 'block'
+    },
+    hideSidebar () {
+      this.$refs.sidebar.style.left = '-25rem'
+      this.$refs.shadow.style.display = 'none'
     }
   }
 }
@@ -49,6 +92,7 @@ export default {
            left: 1rem;
            top: calc(2rem - 2.5rem/2);
            border: 1px solid #fff;
+           cursor: pointer;
        }
    }
     .home-nav {
@@ -66,6 +110,84 @@ export default {
           a:hover {
               color: $default-color;
           }
+       }
+   }
+   .shadow {
+       position: absolute;
+       background-color: #2d2727;
+       opacity: .2;
+       top: 0;
+       left: 0;
+       bottom: 0;
+       right: 0;
+       z-index: 1000;
+       display: none;
+   }
+   .sidebar {
+       background-color: #fff;
+       top: 0;
+       bottom: 0;
+       width: 25rem;
+       left: -25rem;
+       position: absolute;
+       z-index: 1001;
+       transition: left .2s ease-in-out;
+       .sidebar-header {
+           position: relative;
+           width: 100%;
+           padding: 5rem 0 1rem 1rem;
+           background-color: #ccc;
+           color: #fff;
+           overflow: hidden;
+           .cover {
+               position: absolute;
+               top: 0;
+               left: 0;
+               bottom: 0;
+               right: 0;
+               background-color: #eee;
+               opacity: .2;
+               z-index: 1;
+           }
+           .header-top {
+                display: flex;
+                align-items: center;
+                z-index: 3;
+               .sidebar-avatar {
+                   height: 5rem;
+                   width: 5rem;
+                   border-radius: 50%;
+               }
+               .userinfo {
+                   margin-left: 1rem;
+                   h4 {
+                       font-size: 1.5rem;
+                   }
+               }
+           }
+           .header-nav {
+               z-index: 3;
+               display: flex;
+               margin-top: 2rem;
+               justify-content: space-between;
+               align-items: center;
+               width: 100%;
+               .nav-item {
+                  width: 30%;
+                  text-align: center;
+                  .nav-number {
+                      font-size: 1.2rem;
+                  }
+                  .nav-type {
+                      margin-top: 0.5rem;
+                  }
+               }
+               .dash {
+                   width: 1px;
+                   height: 2rem;
+                   background-color: #aaa;
+               }
+           }
        }
    }
 </style>
